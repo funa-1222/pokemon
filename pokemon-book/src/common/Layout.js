76bab0f61f2f5df6d02d6fd5b6ca232e5e19796f
funa-1222 <funabashi-ayaka@gmail.com>
2023-10-27 00:01:46 +0900

    💡

--- pokemon-book/src/common/Layout.js
+++ pokemon-book/src/common/Layout.js
@@ -33,20 +33,21 @@ export default function Layout() {
         component="nav"
         variant="dense"
         sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-      >
+        >
         {sections.map((section) => (
-          <Link
+            //Linkコンポーネントに直したい
+        <Link
             color="inherit"
             noWrap
             key={section.title}
             variant="body2"
             href={section.url}
             sx={{ p: 1, flexShrink: 0 }}
-          >
+        >
             {section.title}
-          </Link>
+        </Link>
         ))}
-      </Toolbar>
+        </Toolbar>
         </AppBar>
         <main>
             <Outlet />
